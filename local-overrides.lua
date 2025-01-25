0a34b4e1de2fc3ee47ade045e190617d51fdb9dc
Luis Armendariz <luis.armendariz@gmail.com>
2025-01-24 17:25:32 -0800

    Add default indentation behavior to local-overrides.lua

--- local-overrides.lua
+++ local-overrides.lua
@@ -1,2 +1,8 @@
 -- My own local overrides
 vim.opt.scrolloff = 3
+
+-- Set default indentation behavior
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
